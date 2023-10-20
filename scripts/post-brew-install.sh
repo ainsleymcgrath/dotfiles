1fc051b7735d2251a69578751c5989039583b28f
Ainz <ainz@ains-mbp.local>
2023-10-19 23:02:37 -0500

    fix: scriptz

--- scripts/post-brew-install.sh
+++ scripts/post-brew-install.sh
@@ -8,11 +8,11 @@ else
 fi
 
 # fzf extras
-$(brew --prefix)/opt/fzf/install --key-bindings --no-completion --no-update-rc  --no-bash --no-zsh --no-fish
+"$(brew --prefix)"/opt/fzf/install --key-bindings --no-completion --no-update-rc  --no-bash --no-zsh --no-fish
+#
 # lunarvim
 if [[ -z $(which lvim) ]]; then
-    LV_BRANCH='release-1.3/neovim-0.9'
-    bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) 
+    LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) 
 else
   echo "LunarVim already installed"
 fi
