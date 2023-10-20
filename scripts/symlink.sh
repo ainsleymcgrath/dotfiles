ac4de322b1568920ddf6cd8b599d120874a27339
Ainz <ainz@ains-mbp.local>
2023-10-19 22:07:14 -0500

    feat: symlinks yay

---
+++ scripts/symlink.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+mkdir -p ~/.config/lvim  # covers ~/.config as well
+
+envsubst < symlinks.txt | while IFS=' | ' read -r source target; do
+  if [[ ! -f "$source" ]]; then
+    echo "❌ No such file '$source'"
+    exit 1
+  fi
+
+  expanded_source=$(readlink -f "$source")
+
+  if [[ -f "$target" ]]; then
+    echo "Symlink from $source to $target already exists."
+  else
+    echo "Linking $expanded_source to $target."
+    ln -s "$expanded_source" "$target"
+  fi
+done
